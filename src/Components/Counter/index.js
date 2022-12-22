007435097f7a786f3f45323b6e0ae889c13b2ea3
[Soumya Ranjan] <soumyaranjan1819@gmail.com>
2022-12-23 00:33:04 +0530

    add,sub with user input value

--- src/Components/Counter/index.js
+++ src/Components/Counter/index.js
@@ -1,19 +1,65 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { increment, decrement, reset } from '../../Features/counterSlice'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByValue,
+  decrementByValue
+} from "../../Features/counterSlice";
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter.count)
-    const dispatch= useDispatch()
-    // console.log(useSelector((state) => state.counter.count))
+  const [value, setValue] = useState(0);
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <>
-        <h1>{count}</h1>
-        <button onClick={()=>{dispatch(increment())}} >+</button>
-        <button onClick={()=>{dispatch(decrement())}} >-</button>
-        <button onClick={()=>{dispatch(reset())}} >Rest</button>
-    </>
-  )
-}
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Rest
+      </button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+      <br />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(incrementByValue(Number(value)));
+        }}
+      >
+        Add Value
+      </button>
 
-export default Counter
+      <button
+        onClick={() => {
+          dispatch(decrementByValue(Number(value)));
+        }}
+      >
+        Subtract Value
+      </button>
+    </>
+  );
+};
+
+export default Counter;
--- src/Features/counterSlice.js
+++ src/Features/counterSlice.js
@@ -1,27 +1,37 @@
-import {createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    count:0
-}
+  count: 0,
+};
 
-export const counterSlice= createSlice({
-    name:'counter',
-    initialState,
-    reducers:{
-        increment:(state)=>{
-            if(state.count<10){
-            state.count +=1;
-            }
-            else
-            alert('Reached maximum limit')
-        },
-        decrement:(state)=>{
-            state.count -=1;
-        },
-        reset:(state)=>{
-            state.count=0;
-        }
-    }
-})
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      if (state.count < 10) {
+        state.count += 1;
+      } else alert("Reached maximum limit");
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByValue: (state, action) => {
+      state.count += action.payload;
+    },
+    decrementByValue: (state, action) => {
+      state.count -= action.payload;
+    },
+  },
+});
 
-export const {increment, decrement, reset}= counterSlice.actions
-export default counterSlice.reducer
+export const {
+  increment,
+  decrement,
+  reset,
+  incrementByValue,
+  decrementByValue,
+} = counterSlice.actions;
+export default counterSlice.reducer;
